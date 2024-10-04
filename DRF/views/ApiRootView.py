d8d5e35c278251bc1f1e4f8ea190e69872123a2d
RSNF <ray1001novaes@gmail.com>
2024-10-04 13:52:06 -0300

    add api-root

---
+++ DRF/views/ApiRootView.py
@@ -0,0 +1,25 @@
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import generics
+
+from drones.views import DroneViewSet
+
+class ApiRoot(generics.GenericAPIView):
+    name = "api-root"
+
+    def get(self, request, *args, **kwargs):
+        return Response(
+            {
+                "drones": {
+                    "drone-categories": reverse("drones:dronecategory-list", request=request),
+                    "drones": reverse("drones:drones-list", request=request),
+                    "pilots": reverse("drones:pilots-list", request=request),
+                    "competitions": reverse("drones:competitions-list", request=request),
+                },
+                "core": {
+                    "livros": reverse("core:livros-list", request=request),
+                    "autores": reverse("core:autores-list", request=request),
+                    "categorias": reverse("core:categorias-list", request=request),
+                }
+            }
+        )
